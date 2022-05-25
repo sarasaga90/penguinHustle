73580a23e26ef300320aa3048871a09d26cbb7b0
sarasaga90 <104619270+sarasaga90@users.noreply.github.com>
2022-05-25 09:58:55 +0200

    canvis menors al main

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 let config = {
     type: Phaser.AUTO,
-    //width: 800,
-    //height: 600,
     width: 960,
     height: 320,
-    // VIGILA!!!! SI ET DEIXES PART DLE CODI, LES PROPIETATS NO CARREGUEN!!!!
     physics: {
         default: 'arcade',
         arcade: {
@@ -19,38 +16,7 @@ let config = {
      
 let game = new Phaser.Game(config);
 
-/*Config música
-var musicConfig = {
-    mute: false,
-    volume: 1,
-    rate: 1,
-    detune: 0,
-    seek: 0,
-    loop: true,
-    delay: false,
-}
-
 //Variables d'escena
 var gameOver;
-var startGame;*/
-
-/* 
-ENLLOC D'UTILITZAR VARIABLES, SOC MÉS FAN D'UTILITZAR EL THIS. COM VEURÀS EN EL CODI, 
-EN AQUELLES ESCENES EN QUE AQUESTA VARIABLE S'UTILITZA MÉS DUN COP 
-*/
-
-/*
-var player;
-var stars;
-var bombs;
-var platforms;
-var cursors;
-var score = 0;
-var gameOver = false;
-var scoreText;
-//extra disparar
-var bullets;
-var lastFired = 0;
-var cursors;
-var fire;
-*/
+var isLevelComplete;
+var gameClear;
